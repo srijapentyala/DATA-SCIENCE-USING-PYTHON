a7a69d945d1ed1d171035169b789911ec7e24aa4
srijapentyala <86046821+srijapentyala@users.noreply.github.com>
2022-06-07 23:14:38 +0530

    Add files via upload

---
+++ multiplication_tables.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+#multiplication table from 1-10
+for i in range(1,11):
+    for j in range(1,11):
+        print(str(i)+"*"+str(j)+"="+str(i*j))
+    print(" ")
+
+
+# In[ ]:
+
+
+
+
